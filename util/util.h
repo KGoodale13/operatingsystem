f1c2a66d9df776581dc11cc2ae6ad45e53008531
Kyle Goodale <KGoodale13@users.noreply.github.com>
2017-10-05 20:15:45 -0400

    Added a util file so I can add any handy functions used my multiple files there if needed

---
+++ util/util.h
@@ -0,0 +1,18 @@
+//
+// Created by Kyle Goodale on 10/3/17.
+//
+
+#ifndef OPERATINGSYSTEM_UTIL_H
+#define OPERATINGSYSTEM_UTIL_H
+
+
+/**
+ * Coverts however many values passed as len starting at start to one number.
+ * @param arr - The array to parse values from
+ * @param start - The starting index
+ * @param len  - How many indexes to include
+ * @return An int representing the values at those indexes i.e [3,4] = 34
+ */
+int parseArray( const char arr[], int start, int len );
+
+#endif OPERATINGSYSTEM_UTIL_H
