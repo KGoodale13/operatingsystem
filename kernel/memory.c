789c0981c0cc9bcc52fcad7c801ebd870078e2c1
Kyle Goodale <KGoodale13@users.noreply.github.com>
2017-10-05 20:14:07 -0400

    Updated memory to support the new PCB and execution contexts.

--- kernel/memory.c
+++ kernel/memory.c
@@ -1,110 +1,27 @@
 // Created by Kyle Goodale. See header for details
-#include <math.h>
 #include <printf.h>
 #include <string.h>
+#include "memory.h"
+#include "../util/util.h"
 
-char memory[ 100 ][ 6 ];
-short int PC = 0;
-short int P0, P1, P2, P3 = 0;
-int R0, R1, R2, R3, ACC = 0;
-char PSW[2], SP[2], IR[6];
+
+static char memory[ MEMORY_LINES ][ 6 ]; // Our operating systems main memory. Shared by all programs
 
 
 // Pretty prints the current state of memory to console
-void dumpMemory( ) {
+void _debug_dumpMemory( ) {
 
     printf( "Main memory dump: \n " );
-    for (int line = 0; line < 100; ++line ) {
-        printf("|%.2i|%.6s|\n ", line, memory[line] );
+    for ( int line = 0; line < MEMORY_LINES; ++line ) {
+        printf("|%.2i|%.6s|\n ", line, memory[ line ] );
     }
 }
 
-// Converts multiple consecutive indexes in an array to a single value.
-int parseArray( const char arr[], int start, int len ) {
-    int value = 0;
-
-    for( int i=start; i < start + len; ++i ) {
-        value += ( arr[ i ] - 48 ) * (int) pow( 10, len - ( i-start ) - 1 );
-    }
-    return value;
-}
-
-// Retrieves the memory location stored at a pointer
-int getPointer( int index ){
-     switch( index ){
-         case 0: return P0;
-         case 1: return P1;
-         case 2: return P2;
-         case 3: return P3;
-         default:
-             printf("Error: Attempted to access invalid pointer using getPointer. Expected (0-3), Got: %i\n", index);
-             return -1;
-     }
-}
-
-// Store a memory location in a pointer
-int setPointer( int index, int value ){
-    if( value < 0 || value > 99 ){
-        printf("Error: Attempted to write to invalid pointer value. Valid locations are 0-99, Got: %i\n", value);
-        return -1;
-    }
-
-    switch( index ){
-        case 0: P0 = (short int) value; break;
-        case 1: P1 = (short int) value; break;
-        case 2: P2 = (short int) value; break;
-        case 3: P3 = (short int) value; break;
-        default:
-            printf("Error: Attempted to write invalid memory address to pointer. Expected (0-3), Got: %i\n", index);
-            return -1;
-    }
-    return 0;
-}
-
-int adjustPointer( int index, int amount ) {
-    int pointerVal = getPointer( index );
-    if( pointerVal == -1 ){
-        printf("Error: Attempted to adjust value of invalid pointer. Expected (0-3), Got: %i\n", index);
-    }
-    return setPointer( index, pointerVal + amount );
-}
-
-// Gets the value stored in a register
-int getRegister( int index ){
-    switch( index ){
-        case 0: return R0;
-        case 1: return R1;
-        case 2: return R2;
-        case 3: return R3;
-        default:
-            printf("Error: Attempted to access invalid register index. Expected (0-3) Got: %i\n", index);
-            return -1;
-    }
-}
-
-// Stores a value in a register
-int setRegister( int index, int value ){
-    if( value < 0 || value > 2147483647 ){
-        printf("Error: Attempted to write to invalid value to register. Expected (0-INT_MAX) Got: %i\n", value);
-        return -1;
-    }
-
-    switch( index ){
-        case 0: R0 = (short int) value; break;
-        case 1: R1 = (short int) value; break;
-        case 2: R2 = (short int) value; break;
-        case 3: R3 = (short int) value; break;
-        default:
-            printf("Error: Attempted to access invalid pointer using getPointer. Expected (0-3), Got: %i\n", index);
-            return -1;
-    }
-    return 0;
-}
 
 // Writes a number to a memory location in the format ZZXXXX where X:{0-9}
-int writeValueToMemory( int location, int value ){
-    if( location < 0 || location > 99 ){
-        printf("Error: Attempted to write value to invalid memory location. Expected (0-99), Got: %i\n", location);
+int _memoryWriteValue( int location, int value ){
+    if( location < 0 || location > MEMORY_LINES ) {
+        printf("Error: Attempted to write value to invalid memory location. Expected (0-%i), Got: %i\n", MEMORY_LINES, location);
         return -1;
     }
 
@@ -124,14 +41,14 @@ int writeValueToMemory( int location, int value ){
     // Z padding is for when storing to memory we pad the first two indexes with ZZ i.e ZZ1234 vs setting the accumulator which has no padding
 
     char newValue[ 6 ] = { 'Z', 'Z', (char)('0' + thousands), (char)('0' + hundreds), (char)('0' + tens), (char)('0' + ones) };
-    memcpy( memory[ location ], newValue , 6 );
+    memcpy( memory[ location ], newValue, 6 );
     return 0;
 }
 
 // Reads a number from memory that is in the format ZZXXXX where X:{0-9}
-int readValueFromMemory( int location ) {
-    if( location < 0 || location > 99 ) {
-        printf("Error: Attempted to read value from invalid memory location. Expected (0-99), Got: %i\n", location);
+int _memoryReadValue( int location ) {
+    if( location < 0 || location > MEMORY_LINES ) {
+        printf("Error: Attempted to read value from invalid memory location. Expected (0-%i), Got: %i\n", MEMORY_LINES, location);
         return -1;
     }
     if( strncmp( &memory[ location ][0], "Z", 1 ) != 0 ) {
@@ -142,57 +59,24 @@ int readValueFromMemory( int location ) {
     return parseArray( memory[ location ], 2, 4 );
 }
 
-// Gets the value stored in memory that the pointer points to
-int getPointerMemoryValue( int index ){
-    return readValueFromMemory( getPointer( index ) );
-}
-
-// Sets the accumulators value
-int setAccumulator( int value ) {
-    if( value < 0 || value > 2147483647 ){
-        printf("Error: Attempted to assign out of range value to the accumulator. Max: 2147483647 Got: %i\n", value);
+// Writes a character array directly into memory.
+int _memoryWriteLine( int location, char *line ){
+    if( location < 0 || location >= MEMORY_LINES ){
+        printf( "Attempting to write line to invalid memory location. Expected(0-%i) Got: %i\n", MEMORY_LINES, location );
         return -1;
     }
-    ACC = value;
+    strcpy( memory[ location ], line );
     return 0;
 }
 
-// Adds the value passed to the accumulator
-int addAccumulator( int amount ){
-    return setAccumulator( ACC + amount );
-}
-
-// subtracts the value passed from the accumulator
-int subtractAccumulator( int amount ){
-    return setAccumulator( ACC - amount );
+// Reads a line directly from the memory table, doesn't format or process it at all
+char *_memoryReadLine( int location ){
+    return memory[location];
 }
 
 
-int getAccumulator( ){
-    return ACC;
+// Clears 'lines' number of lines starting from 'fromLocation'
+int _memoryClearBlock( int fromLocation, int lines ){
+    memset( memory, fromLocation, lines * sizeof( memory[0] ) );
 }
 
-// Sets PSW[0] only for now
-int setPSW( int bool ){
-    if( bool != 1 && bool != 0 ){
-        printf("Error: Invalid value passed to setPSW. Expected 1 or 0, Got: %i\n", bool);
-        return -1;
-    }
-    PSW[0] = (char)(bool + '0');
-    return 0;
-}
-
-// Returns PSW[0] only for now
-int getPSW(){
-    return parseArray( PSW, 0, 1 );
-}
-
-// Sets the program counter to the line passed
-int branch( short int line ){
-    if( line < 0 || line > 99 ){
-        printf("Error: Invalid line passed to branch. Expected 0-99, Got: %i\n", line);
-        return -1;
-    }
-    PC = line - 1; // Because we increase the PC after this opcode completes
-    return 0;
-}
--- kernel/memory.h
+++ kernel/memory.h
@@ -1,6 +1,9 @@
 /**
  * Handles all operations having to do with the os memory.
  * Provides a lot of helper functions for interacting with memory and the registers
+ * These operations are from the perspective on the whole OS memory array and don't take the
+ * current execution context into consideration. To interact with memory using a programs execution context
+ * use the methods found in executionContext.h
  * @author: Kyle Goodale
  * @date: 09/06/2017
  */
@@ -9,133 +12,47 @@
 #ifndef OPERATINGSYSTEM_MEMORY_H
 #define OPERATINGSYSTEM_MEMORY_H
 
-#endif //OPERATINGSYSTEM_MEMORY_H
-
-// Main memory
-extern char memory[][6];
-
-// Operating System Registers:
-extern short int PC; // Program counter
-extern short int P0, P1, P2, P3; // Pointer registers
-extern int R0, R1, R2, R3, ACC; // Registers and accumulator
-extern char PSW[2], SP[2], IR[6];
-
-
-/**
- * @param index - The index of the pointer you want. 0-3
- * @return The value stored at the pointer or -1 if error
- */
-int getPointer( int index );
-
-/**
- * @param index - The index of the pointer you want. 0-3
- * @param value - The value to store in the pointer. This should be a memory location.
- * @return 0 on success -1 on error
- */
-int setPointer( int index, int value );
-
-/**
- * @param index - The index of the pointer you want. 0-3
- * @param value - The value to adjust the pointer value by.
- * @return 0 on success -1 on error
- */
-int adjustPointer( int index, int amount );
-
-/**
- * @param index - The index of the register you want. 0-3
- * @return The value stored at the register or -1 if error
- */
-int getRegister( int index );
-
-/**
- * @param index - The index of the register you want. 0-3
- * @param value - The value to store in the register.
- * @return 0 on success -1 on error
- */
-int setRegister( int index, int value );
-
-/**
- * Writes the passed number to the memory location specified
- * @param location - The line of memory you want to write to. 0-99
- * @param value - The value to store at this location. int of max size 9999
- * @return 0 on success -1 on error
- */
-int writeValueToMemory( int location, int value);
-
-/**
- * Reads a value stored in memory. i.e ZZ3000
- * @param location - The line of memory you want to read from
- * @return The value stored there or -1 on error
- */
-int readValueFromMemory( int location );
-
-/**
- * Reads a value stored in memory at the location the pointer has stored
- * @param index - The pointer index we should use for getting a memory location
- * @return The value stored there or -1 on error
- */
-int getPointerMemoryValue( int index );
-
-/**
- * Sets the ACC to the value passed
- * @param value - The value to set the ACC to. MAX 9999
- * @return 0 on success -1 on error
- */
-int setAccumulator( int value );
-
-/**
- * Increments the ACC by the amount passed
- * @param value - The value to increment the ACC by. This will overflow if current_val + value > 9999
- * @return 0 on success -1 on error
- */
-int addAccumulator( int amount );
-
-/**
- * Decrements the ACC by the amount passed
- * @param value - The value to decrement the ACC by. This will overflow if current_val + value > 9999
- * @return 0 on success -1 on error
- */
-int subtractAccumulator( int amount );
-
-/**
- * Gets the value stored in the ACC
- * @return The value currently in the ACC
- */
-int getAccumulator( );
-
-/**
- * Sets the PSW[0] value to either 1 or 0
- * @param bool - The value to set PSW[0] to (1 or 0)
- */
-int setPSW( int boolean );
-
-/**
- * @return - Returns PSW[0] (1 or 0)
- */
-int getPSW();
-
-/**
- * Sets the program counter to the value passed
- * @param value - The value to set PC to
- */
-int branch( int value );
-
-/**
- * Compares the accumulators value to the value passed based on the comparison type enum passed
- * @param value - The value to compare
- * @return 0 -> False, 1 -> True, -1 -> Error
- */
-int accCompare( int value, short int comparisonType );
-
-
-/**
- * Coverts however many values passed as len starting at start to one number.
- * @param arr - The array to parse values from
- * @param start - The starting index
- * @param len  - How many indexes to include
- * @return An int representing the values at those indexes i.e [3,4] = 34
- */
-int parseArray( const char arr[], int start, int len );
+const int MEMORY_LINES = 1000; // How many rows of memory we have
 
 // Dumps main memory to console for debugging purposes
-void dumpMemory();
+void _debug_dumpMemory();
+
+/**
+ * Writes an entire line into memory at the location specified
+ * @param location - The line in memory to write to
+ * @param line - The 6 character array to write into memory
+ * @return 0 on success, -1 on error
+ */
+int _memoryWriteLine( int location, char *line );
+
+/**
+ * Reads a raw line from memory
+ * @param location - The line in memory to write to
+ * @return 0 on success, -1 on error
+ */
+char* _memoryReadLine( int location);
+
+/**
+ * Reads a line of memory as a value, this line must be in the format ZZ0000 -> ZZ9999
+ * @param location - The line in memory to write to
+ * @return 0 on success, -1 on error
+ */
+int _memoryReadValue( int location );
+
+/**
+ * Writes the number value to the memory location in the format ZZ0000 -> ZZ9999
+ * @param location - The line in memory to write to
+ * @param value - The value to write, max 9999
+ * @return 0 on success, -1 on error
+ */
+int _memoryWriteValue( int location, int value );
+
+/**
+ * Clears 'lines' number of lines starting from the 'fromLocation'
+ * @param fromLocation - The start location
+ * @param lines  - The number of lines to clear
+ * @return 0 on success, -1 on error
+ */
+int _memoryClearBlock( int fromLocation, int lines );
+
+#endif OPERATINGSYSTEM_MEMORY_H
