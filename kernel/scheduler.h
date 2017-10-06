f52d32f60baeca73e160f72868bd21fd1463a043
Kyle Goodale <KGoodale13@users.noreply.github.com>
2017-10-05 20:15:09 -0400

    Added outline for the scheduler [WIP]

---
+++ kernel/scheduler.h
@@ -0,0 +1,26 @@
+/**
+ * Handles scheduling of the different processes. So we can load multiple programs into memory and then run a
+ * certain amount of operations on each and keep switching between them until they all complete.
+ * @author: Kyle Goodale
+ * @date: 10/05/2017
+ */
+
+
+#ifndef OPERATINGSYSTEM_SCHEDULER_H
+#define OPERATINGSYSTEM_SCHEDULER_H
+
+/**
+ * Inserts the passed process identified by the passed PID into the execution queue.
+ * @param PID - The PID of the process
+ */
+void Scheduler_Queue_Process( int PID );
+
+/**
+ * Starts running the processes in the queue. Will continue running them until it finishes
+ * or a process throws an error. If a process throws an error the caller can decide what to do and
+ * can continue the queue by calling Start again.
+ * @return 0 on completion of all scheduled processes. -1 if a process throws an error.
+ */
+int Scheduler_Start();
+
+#endif OPERATINGSYSTEM_SCHEDULER_H
